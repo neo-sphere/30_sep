11817ad003dae161eafc92d266613d14a5117cb1
Santosh Purbey <santosh.purbey@logpoint.com>
2018-12-16 18:00:26 +0545

    admin pannel customize

--- apps/quiz/models.py
+++ apps/quiz/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
     name =  models.CharField(max_length=40, unique=True)
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True, unique=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    
     def __str__(self):
         return self.name
-
--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -106,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'ne'
 
 TIME_ZONE = 'Asia/Kathmandu'
 
